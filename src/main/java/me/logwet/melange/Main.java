34387123220512a9fe7d8cab39aa06cafcf23cb8
logwet <logwet@users.noreply.github.com>
2021-12-21 22:10:02 +1100

    Add FlatLaf theming

--- src/main/java/me/logwet/melange/Main.java
+++ src/main/java/me/logwet/melange/Main.java
@@ -1,10 +1,12 @@
 package me.logwet.melange;
 
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import javax.swing.JFrame;
 import me.logwet.melange.gui.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
+        FlatIntelliJLaf.setup();
         JFrame mainFrame = new MainFrame();
         mainFrame.setVisible(true);
     }
